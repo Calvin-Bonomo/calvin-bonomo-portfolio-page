f2dedf72acad2cb9cf571cc575d8a3521cc9ed08
Calvin-Bonomo <64815955+Calvin-Bonomo@users.noreply.github.com>
2021-02-23 08:54:42 -0500

    Added an about and contact section

--- src/App.vue
+++ src/App.vue
@@ -10,18 +10,17 @@
           <!-- Iterate through items in the toolbar -->
           <v-divider
             vertical
-            v-if="item.title != 'Featured Projects'"
-          ></v-divider>
+            v-if="item.title != 'Featured Projects'">
+          </v-divider>
           <!-- Add a divider in front of every button except the first one -->
           <v-hover v-slot="{ hover }">
-            <v-btn tile text>
+            <v-btn tile text @click="$vuetify.goTo(item.goto)">
               <v-icon tile class="accent--text">{{ item.icon }}</v-icon>
               <v-expand-x-transition>
                 <v-list-item-title
                   v-if="hover"
                   class="transition-fast-in-fast-out secondary--text"
-                  >{{ item.title }}</v-list-item-title
-                >
+                  >{{ item.title }}</v-list-item-title>
                 <!-- Expand the button if the user is hovering over it -->
               </v-expand-x-transition>
             </v-btn>
@@ -29,10 +28,19 @@
         </v-toolbar-items>
       </v-app-bar>
     </v-card>
+    <v-btn
+    icon
+    large
+    v-scroll="onTop"
+    v-show="scrollToTopVis"
+    class="goToTopButton float-right"
+    @click="$vuetify.goTo('#top')">
+      <v-icon>mdi-chevron-up-circle-outline</v-icon>
+    </v-btn>
     <!-- End of toolbar -->
     <span class="bg">
-      <v-main>
-        <v-div class="mainContainer">
+      <v-main id="main">
+        <v-div class="mainContainer" id="top">
           <!-- Main page container -->
           <v-row align="center" justify="center" class="titleCard">
             <!-- Landing page -->
@@ -41,19 +49,27 @@
             </v-col>
           </v-row>
           <!-- End of landing page -->
-          <v-layout class="blurEffect" justify="left"> <!-- Featured projects carousel -->
-            <v-carousel class="featuresCarousel">
+          <v-layout class="blurEffect" justify="left" id="projects"> <!-- Featured projects carousel -->
+            <v-carousel hide-delimiters class="featuresCarousel">
               <v-carousel-item
               v-for="project in featuredProjects"
-              :key="project.id"
+              :key="project.title"
               :src="project.image">
               <div class="carouselInfo" justify="center">
-                <div>{{ project.title }}</div>
-                <div>{{ project.description }}</div>
+                <div class="carouselProjectTitle">{{ project.title }}</div>
+                <div class="carouselProjectDesc">{{ project.description }}</div>
               </div>
               </v-carousel-item>
             </v-carousel>
-          </v-layout>
+          </v-layout> <!-- End of featured projects carousel -->
+          <div class="aboutSection" id="about">
+            <p class="aboutText">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div>
+            <v-card class="contactCard">
+              <v-icon x-large>mdi-github</v-icon>
+            </v-card>
+          </div>
         </v-div>
         <!-- End of main page container -->
       </v-main>
@@ -65,16 +81,24 @@
 export default {
   data() {
     return {
+      scrollToTopVis: false,
       menuItems: [
-        { icon: "mdi-star-four-points-outline", title: "Featured Projects", goto: "" },
-        { icon: "mdi-information-outline", title: "About", goto: "" },
-        { icon: "mdi-at", title: "Contact", goto: "" },
+        { icon: "mdi-star-four-points-outline", title: "Featured Projects", goto: "#projects" },
+        { icon: "mdi-information-outline", title: "About", goto: "#about" },
+        { icon: "mdi-at", title: "Contact", goto: "#contact" },
       ],
       featuredProjects: [
-        { image: "https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png", id: "sdadw", title: "Test", description: "This is a test." }
+        { image: "https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png", title: "Test", description: "This is a test." }
       ]
     };
   },
+  methods: {
+    onTop () {
+      if (typeof window === 'undefined') { return; }
+      const top = window.pageYOffset;
+      this.scrollToTopVis = (top > 100);
+    }
+  }
 };
 </script>
 
@@ -92,6 +116,13 @@ export default {
   height: 100vh;
 }
 
+.goToTopButton {
+  margin-top: 8vh;
+  position: fixed;
+  z-index: 950;
+  right: 1vw;
+}
+
 .titleCard {
   height: 100vh;
 }
@@ -104,18 +135,49 @@ export default {
   color: rgba(255, 255, 255, 0.2);
   backdrop-filter: blur(2px);
   height:100vh;
+  z-index: 1000;
 }
 
 .featuresCarousel {
   margin-top: 30vh;
-  margin-bottom: 30vh;
+  margin-bottom: 10vh;
+  margin-right: 15vw;
+  margin-left: 15vw;
   height: 40vh;
 }
 
 .carouselInfo {
       position: absolute;
-      bottom: 10%;
       background-color: rgba(0, 0, 0, 0.3);
+      bottom: 0;
       padding: 15px;
+}
+
+.carouselProjectTitle {
+  font-size: 2vh;
+}
+
+.carouselProjectDesc {
+  font-size: 90%;
+}
+
+.aboutSection {
+  margin-right: 30vw;
+  margin-left: 30vw;
+  margin-bottom: 10vh;
+  margin-top: -5vh;
+  line-height: 30px;
+  z-index: 900;
+}
+
+.aboutText {
+  border-style: solid;
+  padding: 5px;
+  text-align: center;
+  z-index: inherit;
+}
+
+.contactCard {
+  height: 10vh;
 }
 </style>
